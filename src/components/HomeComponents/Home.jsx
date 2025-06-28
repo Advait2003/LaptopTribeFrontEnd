04bf2f1179d52f0f6bfa3a6bf1ecfbccbd122227
advait <advaits2003@gmail.com>
2025-06-28 17:49:00 +0530

    changes

--- src/components/HomeComponents/Home.jsx
+++ src/components/HomeComponents/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import LatestLaptops from "./LatestLaptops";
 import LatestReviews from "./LatestReviews";
 // import Footer from "../FooterComponents/Footer";
@@ -11,31 +10,14 @@ const Home = () => {
         <div className="container mx-auto px-4 ">
           {/* Subheadline */}
           <p className="text-4xl text-white mb-4 px-10 ">
-=======
-import ManufacturersSection from "./ManufacSection";
-import LatestReviews from "./LatestReviews";
-const Home = () => {
-  return (
-    <div className="bg-gray-700 min-h-screen rounded-lg">
-      {/* <Navbar /> */}
-      <section className="bg-grey-700 text-center py-5">
-        <div className="container mx-auto px-4 ">
-          {/* Subheadline */}
-          <p className="text-4xl text-white  mb-4 px-10 ">
->>>>>>> 94b0eceb4fdc6bfe5544ae39784cea28a3c60c69
             Discover authentic feedback from laptop owners and make an
             informed decision before investing in your next device!
           </p>
         </div>
       </section>
-<<<<<<< HEAD
       <LatestLaptops/>
       <LatestReviews/>
       {/* <Footer/> */}
-=======
-      <ManufacturersSection />
-      <LatestReviews/>
->>>>>>> 94b0eceb4fdc6bfe5544ae39784cea28a3c60c69
     </div>
   );
 };
